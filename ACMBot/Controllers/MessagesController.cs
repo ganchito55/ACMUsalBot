305903a7c4fd7d62faf4d19cac04211655015277
Jorge Durán <ganchito55@gmail.com>
2017-04-29 13:59:00 +0200

    v1

---
+++ ACMBot/Controllers/MessagesController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+
+namespace ACMBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        ///     POST: api/Messages
+        ///     Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            // Only works in ACM Offtopic
+            if (activity.Conversation.Id == Private.ACMOfftopic)
+            {
+                if (activity.Type == ActivityTypes.Message)
+                {
+                    var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                    //Filter bot messages
+                    string replyString = null;
+                    switch (activity.Text)
+                    {
+                        case "/list":
+                            replyString = Messages.Messages.List();
+                            break;
+                        case "/help":
+                            replyString = Messages.Messages.Help();
+                            break;
+                    }
+
+                    // return our reply to the user
+                    var reply = activity.CreateReply($@"{replyString}");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else
+                {
+                    HandleSystemMessage(activity);
+                }
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            var connector = new ConnectorClient(new Uri(message.ServiceUrl));
+
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+                if (message.MembersAdded.Any())
+                    foreach (var account in message.MembersAdded)
+                    {
+                        var response = message.CreateReply(Messages.Messages.Welcome(account.Name));
+                        connector.Conversations.ReplyToActivityAsync(response);
+                    }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ACMBot/Messages/Messages.cs
@@ -0,0 +1,58 @@
+﻿namespace ACMBot.Messages
+{
+    public class Messages
+    {
+        private Messages()
+        {
+            
+        }
+
+        /// <summary>
+        /// Create welcome message text
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string Welcome(string name)
+        {
+            return $@"**Hola {name}, bienvenido a la ACM USAL!**
+
+Se te ha dado de alta en dos grupos, este y ACM-Offtopic.
+
+Este grupo es para cosas importantes, que todos debemos saber.
+Estaría bien que dejaras este grupo con notificaciones.
+
+El otro grupo es para que hables siempre que quieras sobre cualquier
+cosa que se te ocurra, y lo puedes silenciar si te molesta demasiado.
+
+Si necesitas saber cuales son los grupos, lo puedes hacer con:
+/list
+
+Encantado de tenerte por aquí!";
+        }
+
+        /// <summary>
+        /// Help message
+        /// </summary>
+        /// <returns></returns>
+        public static string Help()
+        {
+            return @"Este bot permite añadirte a los grupos SIG de ACM.
+
+Lista de comandos:
+
+**/list** Lista los grupos
+
+**/help** Muestra este mensaje";
+        }
+
+        /// <summary>
+        /// Show all groups
+        /// </summary>
+        /// <returns></returns>
+        public static string List()
+        {
+            return $@"ACM-SIGNET [entrar]({Private.SIGNET})";
+        }
+
+    }
+}
